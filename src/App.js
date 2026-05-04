f81fc1102a2f23004215617a6bd2adb5d6844775
Moussa-Ndioukh-Faye <fayemoussandioukh@gmail.com>
2026-05-04 15:59:49 +0000

    Correction conflit CSS

--- src/App.js
+++ src/App.js
@@ -1,19 +1,80 @@
 import './App.css';
 import Header from './Header';
-<<<<<<< HEAD
 import ListeLignes from './ListeLignes';
 import Footer from './Footer';
 
 function App() {
+ 
   const lignes = [
-    { id: 1, numero: "1", depart: "Parcelles Assainies", arrivee: "Plateau", arrets: 14 },
-    { id: 2, numero: "7", depart: "Guédiawaye", arrivee: "Place Obélisque", arrets: 18 },
-    { id: 3, numero: "15", depart: "Pikine", arrivee: "Médina", arrets: 12 },
-    { id: 4, numero: "23", depart: "Ouakam", arrivee: "Grand Dakar", arrets: 10 },
-    { id: 5, numero: "8", depart: "Almadies", arrivee: "Colobane", arrets: 16 },
-    { id: 6, numero: "12", depart: "Yoff", arrivee: "Sandaga", arrets: 11 },
-  ];
-
+  {
+    id: 1,
+    numero: "1",
+    depart: "Parcelles Assainies",
+    arrivee: "Plateau",
+    listeArrets: [
+      "Parcelles U14", "Parcelles U10",
+      "Camberene", "Patte d'Oie",
+      "Grand Dakar", "Colobane",
+      "Ponty", "Plateau"
+    ]
+  },
+  {
+    id: 2,
+    numero: "7",
+    depart: "Guediawaye",
+    arrivee: "Place Obe",
+    listeArrets: [
+      "Guediawaye", "Pikine",
+      "Thiaroye", "Keur Massar",
+      "Grand Yoff", "Parcelles",
+      "Liberte 6", "Place Obe"
+    ]
+  },
+  {
+    id: 3,
+    numero: "15",
+    depart: "Pikine",
+    arrivee: "Medina",
+    listeArrets: [
+      "Pikine Centre", "Thiaroye Gare",
+      "Hann", "Colobane",
+      "Fass", "Medina"
+    ]
+  },
+  {
+    id: 4,
+    numero: "23",
+    depart: "Ouakam",
+    arrivee: "Grand Dakar",
+    listeArrets: [
+      "Ouakam Village", "Mermoz",
+      "Fann", "Point E",
+      "Liberte 5", "Grand Dakar"
+    ]
+  },
+  {
+    id: 5,
+    numero: "8",
+    depart: "Almadies",
+    arrivee: "Colobane",
+    listeArrets: [
+      "Almadies", "Ngor",
+      "Yoff", "Ouest Foire",
+      "Liberte 6", "Colobane"
+    ]
+  },
+  {
+    id: 6,
+    numero: "12",
+    depart: "Yoff",
+    arrivee: "Sandaga",
+    listeArrets: [
+      "Yoff Village", "Aeroport LSS",
+      "Parcelles U17", "Grand Yoff",
+      "HLM", "Sandaga"
+    ]
+  }
+];
   return (
     <div className="App">
       <Header />
@@ -28,20 +89,4 @@ function App() {
 }
 
 export default App;
-=======
 
-function App() {
-    return (
-        <div className="App">
-            <Header />
-            <main className="contenu">
-                <p>Bienvenue ! Cette application vous permet de trouver les meilleures lignes de transport en commun à Dakar.</p>
-            </main>
-        </div>
-    );
-}
-
-//const App = () => <div className="App">a </div>;
-export default App;
-  
->>>>>>> 5c3a09462e488db665e1d62f36914a8e53c6c715
--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,12 @@
 import './Header.css'
 
-function Header () {
+function Header(){
     return(
         <header className="header">   
             <h1 className='header-titre'>SenTransport</h1>
             <p className="header-soustitre">
-<<<<<<< HEAD
-                Votre guide du transport en commun à Dakar   </p> 
-                {new Date().toLocaleDateString()}        
-=======
-                Votre guide du transport en commun à Dakar  
+                Votre guide de transport à Dakar
             </p>
->>>>>>> 5c3a09462e488db665e1d62f36914a8e53c6c715
         </header>
     );
 }
