44ae212929313e6bb83d7868b601c98786fb8468
Moussa-Ndioukh-Faye <fayemoussandioukh@gmail.com>
2026-05-04 20:22:26 +0000

    Correction: restauration des donnees lignes manquantes

--- src/App.js
+++ src/App.js
@@ -10,7 +10,82 @@ function App() {
   const [recherche, setRecherche] = useState("");
   const [ligneSelectionnee, setLigneSelectionnee] = useState(null);
 
-  const lignes = [ /* ton tableau */ ];
+  const lignes = [
+    {
+      id: 1,
+      numero: "1",
+      depart: "Parcelles Assainies",
+      arrivee: "Plateau",
+      arrets: 8,
+      listeArrets: [
+        "Parcelles U14", "Parcelles U10",
+        "Camberene", "Patte d'Oie",
+        "Grand Dakar", "Colobane",
+        "Ponty", "Plateau"
+      ]
+    },
+    {
+      id: 2,
+      numero: "7",
+      depart: "Guediawaye",
+      arrivee: "Place Obe",
+      arrets: 8,
+      listeArrets: [
+        "Guediawaye", "Pikine",
+        "Thiaroye", "Keur Massar",
+        "Grand Yoff", "Parcelles",
+        "Liberte 6", "Place Obe"
+      ]
+    },
+    {
+      id: 3,
+      numero: "15",
+      depart: "Pikine",
+      arrivee: "Medina",
+      arrets: 6,
+      listeArrets: [
+        "Pikine Centre", "Thiaroye Gare",
+        "Hann", "Colobane",
+        "Fass", "Medina"
+      ]
+    },
+    {
+      id: 4,
+      numero: "23",
+      depart: "Ouakam",
+      arrivee: "Grand Dakar",
+      arrets: 6,
+      listeArrets: [
+        "Ouakam Village", "Mermoz",
+        "Fann", "Point E",
+        "Liberte 5", "Grand Dakar"
+      ]
+    },
+    {
+      id: 5,
+      numero: "8",
+      depart: "Almadies",
+      arrivee: "Colobane",
+      arrets: 6,
+      listeArrets: [
+        "Almadies", "Ngor",
+        "Yoff", "Ouest Foire",
+        "Liberte 6", "Colobane"
+      ]
+    },
+    {
+      id: 6,
+      numero: "12",
+      depart: "Yoff",
+      arrivee: "Sandaga",
+      arrets: 6,
+      listeArrets: [
+        "Yoff Village", "Aeroport LSS",
+        "Parcelles U17", "Grand Yoff",
+        "HLM", "Sandaga"
+      ]
+    }
+  ];
 
   const lignesFiltrees = lignes.filter(l => {
     const search = recherche.toLowerCase();
